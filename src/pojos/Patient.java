9bdc662b01238f097b8b0f8d20d16530323f1954
alba <alba@192.168.1.38>
2020-03-19 14:16:45 +0100

    modified pojos to ad two classes Patient and Symptom

---
+++ src/pojos/Patient.java
@@ -0,0 +1,99 @@
+package pojos;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Patient implements Serializable{
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6669027231906981776L;
+	
+	private Integer id;
+	private String name;
+	private String gender;
+	private String state;
+	private Date dob;
+	
+	public Patient() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + ", name=" + name + ", gender=" + gender + ", state=" + state + ", dob=" + dob
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
+}
---
+++ src/pojos/Symptom.java
@@ -0,0 +1,69 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class Symptom implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2054544053127653172L;
+	
+	private Integer id;
+	private String manifestation;
+	
+	
+	public Symptom() {
+		super();
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getManifestation() {
+		return manifestation;
+	}
+	public void setManifestation(String manifestation) {
+		this.manifestation = manifestation;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Symptom [id=" + id + ", manifestation=" + manifestation + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Symptom other = (Symptom) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
