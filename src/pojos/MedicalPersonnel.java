f1894a116df6489a989cd1b1bdba51f0a8b080ee
Usuario <Usuario@192.168.1.36>
2020-03-19 11:37:08 +0100

    addition of medical personnel and pathology class

---
+++ src/pojos/MedicalPersonnel.java
@@ -0,0 +1,96 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class MedicalPersonnel implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4865330136642653318L;
+
+	private Integer id;
+	private String name;
+	private String department;
+	private String position;
+	private Integer pathologyId;
+	
+	public MedicalPersonnel() {
+		
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public Integer getPathologyId() {
+		return pathologyId;
+	}
+
+	public void setPathologyId(Integer pathologyId) {
+		this.pathologyId = pathologyId;
+	}
+
+	@Override
+	public String toString() {
+		return "MedicalPersonnel [id=" + id + ", name=" + name + ", department=" + department + ", position=" + position
+				+ ", pathologyId=" + pathologyId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MedicalPersonnel other = (MedicalPersonnel) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
---
+++ src/pojos/Pathology.java
@@ -0,0 +1,105 @@
+package pojos;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Pathology implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1289491893153664835L;
+
+	private Integer id;
+	private String name;
+	private Integer duration;
+	private Date startDate;
+	private Date endingDate;
+	
+	public Pathology() {
+		
+		super();
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndingDate() {
+		return endingDate;
+	}
+
+	public void setEndingDate(Date endingDate) {
+		this.endingDate = endingDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Pathology [id=" + id + ", name=" + name + ", duration=" + duration + ", startDate=" + startDate
+				+ ", endingDate=" + endingDate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pathology other = (Pathology) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
