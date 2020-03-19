9fe8777da7a8a5daf7613825a3534cd01a80d03f
pablo <pablo@LAPTOP-PEG82UEF>
2020-03-19 12:19:16 +0100

    Classes Allergy and ClinicalHistory

---
+++ src/pojos/Allergy.java
@@ -0,0 +1,74 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class Allergy implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 230505548360698610L;
+	
+	private Integer id;
+	private String allergy;
+	private Integer degree;
+	
+	public Allergy() {
+		super();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getAllergy() {
+		return allergy;
+	}
+	
+	public void setAllergy(String allergy) {
+		this.allergy = allergy;
+	}
+	
+	public Integer getDegree() {
+		return degree;	
+	}
+	
+	public void setDegree(Integer degree) {
+		this.degree = degree;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Allergy [id=" + id + ", allergy=" + allergy + ", degree=" + degree + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allergy other = (Allergy) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
---
+++ src/pojos/ClinicalHistory.java
@@ -0,0 +1,96 @@
+package pojos;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class ClinicalHistory implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3334006632519187423L;
+	
+	private Integer id;
+	private Date doe;
+	private Date dod;
+	private String bloodType;
+	private String extraInfo;
+	
+	public ClinicalHistory() {
+		super();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Date getDoe() {
+		return doe;
+	}
+
+	public void setDoe(Date doe) {
+		this.doe = doe;
+	}
+
+	public Date getDod() {
+		return dod;
+	}
+
+	public void setDod(Date dod) {
+		this.dod = dod;
+	}
+
+	public String getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(String bloodType) {
+		this.bloodType = bloodType;
+	}
+
+	public String getExtraInfo() {
+		return extraInfo;
+	}
+
+	public void setExtraInfo(String extraInfo) {
+		this.extraInfo = extraInfo;
+	}
+
+	@Override
+	public String toString() {
+		return "ClinicalHistory [id=" + id + ", doe=" + doe + ", dod=" + dod + ", bloodType=" + bloodType
+				+ ", extraInfo=" + extraInfo + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClinicalHistory other = (ClinicalHistory) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+
+}
