6682215d3fe212793c98001804136f79763cc720
alvar <alvar@192.168.1.41>
2020-03-19 19:35:51 +0100

    Treatment pojo addition

---
+++ src/pojos/Treatment.java
@@ -0,0 +1,73 @@
+package pojos;
+
+import java.io.Serializable;
+
+public class Treatment implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9161173435855282694L;
+
+	private Integer id;
+	private String name;
+	private String medication;
+	private String description;
+
+	public Treatment() {
+		super();
+	}
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getMedication() {
+			return medication;
+		}
+		public void setMedication(String medication) {
+			this.medication = medication;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		@Override
+		public String toString() {
+			return "Treatment [id=" + id + ", name=" + name + ", medication=" + medication + ", description="
+					+ description + "]";
+		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Treatment other = (Treatment) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
+		
+}
