f33045729b47987f4711a95e2e31acaa345a6b5b
pablo <pablo@LAPTOP-PEG82UEF>
2020-03-22 20:52:15 +0100

    Añadido de AllergyManager y ClinicalHistoryManager

---
+++ src/db/interfaces/AllergyManager.java
@@ -0,0 +1,15 @@
+package db.interfaces;
+
+//import java.util.List;
+
+//import pojos.Allergy;
+
+public interface AllergyManager {
+
+	//public void add(Allergy allergy);
+	//public void update(Allergy allergy);
+	//public void delete(Allergy allergy);
+	//public List<Allergy> searchAllergyById(Integer id);
+	//public List<Allergy> searchAllergyByAllergy(String allergy);
+	
+}
---
+++ src/db/interfaces/ClinicalHistoryManager.java
@@ -0,0 +1,13 @@
+package db.interfaces;
+
+//import java.util.List;
+
+//import pojos.ClinicalHistory;
+
+public interface ClinicalHistoryManager {
+	//public void add(ClinicalHistory clinicalHistory);
+	//public void update(ClinicalHistory clinicalHistory);
+	//public void delete(ClinicalHistory clinicalHistory);
+	//public List<ClinicalHistory> searchClinicalHistoryById(Integer id);
+
+}
