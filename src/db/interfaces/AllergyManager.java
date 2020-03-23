91e8c1f3d2ebdb3ff13af9646b2b443aec022782
pablo <pablo@LAPTOP-PEG82UEF>
2020-03-23 17:08:50 +0100

    Actualizacion de esto de Allergy y clinicHist

--- src/db/interfaces/AllergyManager.java
+++ src/db/interfaces/AllergyManager.java
@@ -1,15 +1,13 @@
 package db.interfaces;
 
-//import java.util.List;
+import java.util.List;
 
-//import pojos.Allergy;
+import pojos.Allergy;
 
 public interface AllergyManager {
 
-	//public void add(Allergy allergy);
-	//public void update(Allergy allergy);
-	//public void delete(Allergy allergy);
-	//public List<Allergy> searchAllergyById(Integer id);
-	//public List<Allergy> searchAllergyByAllergy(String allergy);
+	public void add(Allergy allergy);
+	public List<Allergy> searchAllergyById(Integer id);
+	public List<Allergy> searchAllergyByAllergy(String allergy);
 	
 }
