bc0c8c37f038cd7041852933190f41392506c102
Usuario <Usuario@192.168.1.36>
2020-03-23 16:50:53 +0100

    sqlite pathology manager

---
+++ src/db/sqlite/SQLitePathologyManager.java
@@ -0,0 +1,40 @@
+package db.sqlite;
+
+import java.util.List;
+
+import db.interfaces.PathologyManager;
+import pojos.Pathology;
+
+public class SQLitePathologyManager implements PathologyManager {
+
+	@Override
+	public void add(Pathology pathology) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Pathology pathology) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Pathology pathology) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Pathology> searchPathologyById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Pathology> searchPathologyByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
