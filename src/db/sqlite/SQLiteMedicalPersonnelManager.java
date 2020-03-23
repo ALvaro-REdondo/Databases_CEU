5a289b99d4ed61fb3389a9df32cf2ed81804d5f9
Usuario <Usuario@192.168.1.36>
2020-03-23 17:03:42 +0100

    slqlite medical personnel

---
+++ src/db/sqlite/SQLiteMedicalPersonnelManager.java
@@ -0,0 +1,40 @@
+package db.sqlite;
+
+import java.util.List;
+
+import db.interfaces.MedicalPersonnelManager;
+import pojos.MedicalPersonnel;
+
+public class SQLiteMedicalPersonnelManager implements MedicalPersonnelManager {
+
+	@Override
+	public void add(MedicalPersonnel medicalPersonnel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(MedicalPersonnel medicalPersonnel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(MedicalPersonnel medicalPersonnel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<MedicalPersonnel> searchMedicalPersonnelById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MedicalPersonnel> searchMedicalPersonnelByPathologyId(Integer pathologyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
