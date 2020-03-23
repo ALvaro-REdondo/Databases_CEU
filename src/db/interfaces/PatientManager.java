cd919baee64d492551f4542895e954ef8920c160
alba <alba@192.168.1.38>
2020-03-23 10:08:48 +0100

    Added to db.interfaces PatientManager and SymptomMangaer

--- src/db/interfaces/PatientManager.java
+++ src/db/interfaces/PatientManager.java
@@ -1,5 +1,14 @@
 package db.interfaces;
 
+import pojos.Patient;
+import java.util.List;
+
 public interface PatientManager {
+	
+		public void add(Patient patient);
+		public void update(Patient patient);
+		public void delete(Patient patient);
+		public List<Patient> searchPatientById(Integer id);
+		public List<Patient> searchâtientByName(String name);
 
 }
--- src/db/interfaces/SymptomMangager.java
+++ src/db/interfaces/SymptomMangager.java
@@ -1,5 +1,14 @@
 package db.interfaces;
 
+//import pojos.Symptom;
+//import java.util.List;
+
 public interface SymptomMangager {
+	
+		//public void add(Symptom symptom);
+		//public void update(Symptom symptom);
+		//public void delete(Symptom symptom);
+		//public List<Symptom> searchSymptomById(Integer id);
+		//public List<Symtptom> searchASymptomByManifestation(String manifestation);
 
 }
