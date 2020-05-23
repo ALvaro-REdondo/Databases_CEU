54c1373f5da2eddcbf29ef37735ffc45896de20d
pablo <pablo@LAPTOP-PEG82UEF>
2020-05-24 01:54:50 +0200

    Fixed the JPA pojos of Allergy and ClinicalHistory

--- src/db/interfaces/AllergyJPAManager.java
+++ src/db/interfaces/AllergyJPAManager.java
@@ -1,5 +1,14 @@
 package db.interfaces;
 
-public interface AllergyJPAManager {
+import java.util.List;
 
+import pojos.Allergy;
+
+public interface AllergyJPAManager {
+	
+	public void connect();
+	public void disconnect();
+	public void createAllergy(Allergy allergy);
+	public Allergy getAllergy(int id);
+	public List<Allergy> getAllergies();
 }
