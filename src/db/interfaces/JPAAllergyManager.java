33c62776894617117907f24fcf112e442a742285
pablo <pablo@LAPTOP-PEG82UEF>
2020-05-30 11:56:33 +0200

    added package xml.utils

--- src/db/interfaces/JPAAllergyManager.java
+++ src/db/interfaces/JPAAllergyManager.java
@@ -17,7 +17,7 @@ public class JPAAllergyManager implements AllergyJPAManager {
 	public void connect() {
 		// HAY QUE CAMBIAR LO DE COMPANY-PROVIDER
 		// REALMENTE NO SE SI TENGO QUE HACER LO DE CONNECT Y DISCONNECT
-		em = Persistence.createEntityManagerFactory("allergy").createEntityManager();
+		em = Persistence.createEntityManagerFactory("Clinical_trials_provider").createEntityManager();
 		em.getTransaction().begin();
 		em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
 		em.getTransaction().commit();
