cd48cf24dcf5277c2c06003e07331314636eb4ee
alvar <alvar@192.168.1.43>
2020-05-30 11:17:43 +0200

    Role and user creation

--- src/pojos/users/User.java
+++ src/pojos/users/User.java
@@ -1,5 +1,85 @@
 package pojos.users;
 
-public class User {
+import java.util.Arrays;
+import javax.persistence.*;
+import java.io.Serializable;
 
+@Entity
+@Table(name = "users")
+public class User implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4356617393160006653L;
+	
+	@Id
+	@GeneratedValue(generator="users")
+	@TableGenerator(name="users", table="sqlite_sequence",
+	pkColumnName = "name",valueColumnName = "seq", pkColumnValue = "users" )
+	private Integer id;
+	private String username;
+	@Lob
+	private byte[] password;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "role_id")
+	private Role role;
+	
+	
+	public User() {
+		super();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public byte[] getPassword() {
+		return password;
+	}
+	public void setPassword(byte[] password) {
+		this.password = password;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + Arrays.toString(password) + ", role="
+				+ role + "]";
+	}
+	
+	
 }
