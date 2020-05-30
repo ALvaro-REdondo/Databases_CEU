e6560d64d6e1d4780b3999ef28d5a7ba9445cdce
alvar <alvar@192.168.1.43>
2020-05-30 10:38:16 +0200

    JPATreatmentManager

---
+++ src/db/interfaces/JPATreatmentManager.java
@@ -0,0 +1,13 @@
+package db.interfaces;
+
+import java.util.List;
+
+import pojos.*;
+
+public interface JPATreatmentManager {
+	public void add(Treatment treatment);
+	public void update(Treatment treatment);
+	public void delete(Treatment treatment);
+	public Treatment searchTreatmentById(Integer id);
+	public List<Treatment> searchTreatmentByName(String name);
+}
