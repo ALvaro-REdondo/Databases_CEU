d1058df3a2333d139b8b2513b70f48d1102438c8
nahiagi <nahiagi00@gmail.com>
2020-05-30 09:51:12 +0200

    manager de medicalpersonnel

--- src/db/interfaces/MedicalPersonnelJPAManager.java
+++ src/db/interfaces/MedicalPersonnelJPAManager.java
@@ -10,10 +10,10 @@ public interface MedicalPersonnelJPAManager {
 	
 	public void connect();
 	public void disconnect();
-	public void createMedicalPersonnel(MedicalPersonnel medicalPersonnel);
+	public void createMedicalPersonnel(MedicalPersonnel_JPA medicalPersonnel);
 	public MedicalPersonnel_JPA getMedicalPersonnel(int id);
 	public List<MedicalPersonnel_JPA> getMedicalPersonnels();
-	public void deleteMedicalPersonnel();
-	public void updateMedicalPersonnel();	
+	public void deleteMedicalPersonnel(MedicalPersonnel_JPA medicalPersonnel);
+	public void updateMedicalPersonnel(MedicalPersonnel_JPA medicalPersonnel);	
 	
 }
